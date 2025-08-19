c5d4f6220467e8842db5803ec003303e298f42db
Akshatsah04 <iamakshatsah@gmail.com>
2025-08-19 22:52:33 +0530

    lab2second

---
+++ lab2/src/second.java
@@ -0,0 +1,30 @@
+public class second {
+        static boolean arms(int num){
+            int count = 0;
+            int temp = num;
+            int temp2 = num;
+            while (temp>0){
+                count++;
+                temp/=10;
+            }
+            double sum =0;
+            while (temp2>0){
+                int dig = temp2%10 ;
+                sum += Math.pow(dig , count);
+                temp2/=10;
+            }
+            if(num == sum){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+    public static void main(String[] args){
+        for (int i =0 ; i<1000 ; i++){
+            if (arms(i)){
+                System.out.println(i +" ");
+            }
+        }
+    }
+}
