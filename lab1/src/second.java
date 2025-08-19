3c1ae84cdae0344058b67ffb8b5b8da6d95dd3e6
Akshatsah04 <iamakshatsah@gmail.com>
2025-08-19 17:21:04 +0530

    second Q

--- lab1/src/second.java
+++ lab1/src/second.java
@@ -8,5 +8,19 @@ public class second {
         int y = scanner.nextInt();
         System.out.println("Enter the third value");
         int z = scanner.nextInt();
+
+        if (x>y){
+            if (x>z) {
+                System.out.println(x + " is the largest number");
+            }else{
+                System.out.println(z + " is the largest number");
+            }
+        }else{
+            if(y>z){
+                System.out.println(y + " is the largest number");
+            }else{
+                System.out.println(z + " is the largest number");
+            }
+        }
     }
 }
