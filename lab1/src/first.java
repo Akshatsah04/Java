b9a30c83f97852f13f3c63f0ad119e0a9a94ce8e
Akshatsah04 <iamakshatsah@gmail.com>
2025-08-19 17:12:31 +0530

    Updated Main.java with new changes

--- lab1/src/first.java
+++ lab1/src/first.java
@@ -5,11 +5,17 @@ public class first {
         System.out.println("Enter the value of radius");
         int rad = scanner.nextInt();
 
-        double peri = 2*Math.PI*rad;
-        System.out.println("the perimeter of the circle is "+ peri);
+        if(rad>0){
+            double peri = 2*Math.PI*rad;
+            System.out.println("the perimeter of the circle is "+ peri);
 
-        double area = Math.PI*rad*rad;
-        System.out.println("the area of the circle "+ area);
+            double area = Math.PI*rad*rad;
+            System.out.println("the area of the circle "+ area);
+        }else{
+            System.out.println(" please enter non zero positive number ");
+        }
+
+
 
         scanner.close();
     }
---
+++ lab1/src/second.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class second {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first value");
+        int x = scanner.nextInt();
+        System.out.println("Enter the second value");
+        int y = scanner.nextInt();
+        System.out.println("Enter the third value");
+        int z = scanner.nextInt();
+    }
+}
